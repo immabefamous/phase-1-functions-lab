79a26bc16b57158e1b45ce5aa5aa63b49cdbb82d
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-21 10:45:30 -0400

    commit tree e9b45978bb7f1896b3921ffebbb7804d362948e7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function  distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return Math.abs(distanceFromHqInBlocks(someValue) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(distanceFromHqInFeet(destination) - distanceFromHqInFeet(start))
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000 && distanceTravelledInFeet(start, destination) > 400) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500 && distanceTravelledInFeet(start, destination) > 2000) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    }
+}
